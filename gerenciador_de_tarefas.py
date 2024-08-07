d7e9c9a897fb4194a5df400e7f21ab18f3478c1c
Luan Lobo <luanfamilia2015@gmail.com>
2024-08-07 15:50:05 -0300

    FASE FINAL/COREÇÃO DE BUGS

--- gerenciador_de_tarefas.py
+++ gerenciador_de_tarefas.py
@@ -1,3 +1,5 @@
+import os
+
 lista_de_tarefas = [{"nome":"limpar casa","status":False},
                     {"nome":"1 hora de video game","status":True}]
 
@@ -8,12 +10,13 @@ def nome_app():
     print("*" * 30)
 
 def menu():
-    print("1-Adicionar tarefa")
-    print("2-Editar Tarefa")
-    print("3-Listar Tarefas")
-    print("4-Remover Tarefa")
-    print("5-Sair")
-    #UM POSSIVEL EDITAR TAREFA
+    print("1- Adicionar tarefa")
+    print("2- Editar Tarefa")
+    print("3- Alterar status da tarefa")
+    print("4- Mostrar Tarefas")
+    print("5- Remover uma Tarefa")
+    print("6- Sair")
+    
 
 def finalizar_app():
     print("Aplicativo encerrado")
@@ -22,26 +25,67 @@ def voltar_ao_menu():
     input("\nDigite qualquer tecla para voltar ao menu: ")
     main()
 
-def adicionar_tarefas():#NÃO ESTA ADICIONANDO
-    adicionar_tarefa = input("Qual tarefa deseja adcionar? ")
-    dados_do_gerenciador = {"nome":"Caminhar","Status":False}
-    lista_de_tarefas.append[dados_do_gerenciador]
-    print(lista_de_tarefas)
+def subtitulo(texto):
+    os.system("cls")
+    linha = "*" * len(texto)
+    print(linha)
+    input(texto)
+    print(linha)
+    print()
+
+def adicionar_tarefas():
+    subtitulo("ADICIONAR UMA TAREFA")
+    adicionarTarefa = input("Digite a tarefa que deseja adicionar: ")
+    dados_gerenciador = {"nome":adicionarTarefa,"status": False}
+    lista_de_tarefas.append(dados_gerenciador)
     voltar_ao_menu()
 
-#def editar_tarefa():
+def alterar_nome_tarefa():
+    subtitulo("ALTERAR NOME DE UMA TAREFA")
+    nome_da_tarefa = input("Infome o nome da tarefa que deseja modificar: ")
+    tarefa_encontrada = False
+
+    for i,tarefa in enumerate(lista_de_tarefas):
+        if nome_da_tarefa == tarefa["nome"]:
+            tarefa_encontrada = True
+            novo_nome_da_tarefa = input("Informe o nome atualizado da tarefa: ")
+            lista_de_tarefas[i]["nome"] = novo_nome_da_tarefa
+            print("Nome alterado com sucesso!")
+            break
+
+        if not tarefa_encontrada:
+            print("Tarefa não encontrada!")
+        voltar_ao_menu()
+
+def status_da_tarefa():
+    subtitulo("STATUS DA TAREFA")
+    nome_tarefa = input("Digite o nome da terefa: ")
+    tarefa_encontrada = False
+
+    for tarefa in lista_de_tarefas:
+        if nome_tarefa == tarefa["nome"]:
+            tarefa_encontrada =True
+            tarefa["status"] = not tarefa["status"]
+            mensagem = f"Tarefa realizada" if tarefa["status"] else "Não realizada"
+            print(mensagem)
+        voltar_ao_menu()
     
 def listar_tarefas():
+    subtitulo("LISTANDO TAREFAS")
+    print(f"{'Nome da Tarefa'.ljust(30)} | {'Status'.ljust(30)}")
     for tarefa in lista_de_tarefas:
         nome_tarefa = tarefa["nome"]
         ativo = "Realizada" if tarefa["status"] else "Não Realizada"
-        print(f"A tarefa {nome_tarefa}, com o Status {ativo} ")
+        print(f"- {nome_tarefa.ljust(30)} | {ativo} ")
     voltar_ao_menu()
 
-def remover_tarefa(): #FALTA ESSA
+def remover_tarefa():
+    subtitulo("REMOVER TAREFAS")
     remover = input("Digite a tarefa que deseja excluir:  ")
-    if lista_de_tarefas == remover:
-        lista_de_tarefas.remove[remover]
+    for tarefa in lista_de_tarefas:
+        if tarefa["nome"] == remover:
+            lista_de_tarefas.remove(tarefa)
+            break
     voltar_ao_menu()
 
 def escolher_opcao():
@@ -51,11 +95,12 @@ def escolher_opcao():
         if opcao_escolhida == 1:
             adicionar_tarefas()
         elif opcao_escolhida == 2:
-            print("Editar tarefa")
-            #editar_tarefa()
+            alterar_nome_tarefa()
         elif opcao_escolhida == 3:
-            listar_tarefas()
+            status_da_tarefa()           
         elif opcao_escolhida == 4:
+            listar_tarefas()
+        elif opcao_escolhida == 5:
             remover_tarefa()
         else:
             finalizar_app()
@@ -66,6 +111,7 @@ def escolher_opcao():
         voltar_ao_menu()
 
 def main():
+    os.system("cls")
     nome_app()
     menu()
     escolher_opcao()
